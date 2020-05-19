bab5dec008e1fb204d0e08804246d55d409229db
josh <josh>
2020-05-18 19:58:02 -0600

    stuck on product api call

--- online-store-react/src/Products.js
+++ online-store-react/src/Products.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
+function getProductsFromAPI(){
+  const request = new XMLHttpRequest();
+  request.open('GET','http://localhost:8080/products',true);
+  request.onload = function(){
+    console.log("something here");
+    const data = JSON.parse(this.response);
+    if(request.status >= 200 && request.status < 400){
+      data.forEach(thing =>{
+        console.log(thing);
+      })
+    }else{
+      console.log("error");
+    }
+  }
+  console.log("chicken tenders");
+  return [];
+}
+
+function getProducts(){
+  const productsFromAPI = getProductsFromAPI();
+  const products = [];
+  for(const [id, name, desc, price] of productsFromAPI){
+    products.push(<tr>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{desc}</td>
+                <td>{price}</td>
+              </tr>);
+  }
+  return products;
+}
 
 class Products extends Component {
 
   render() {
-    const elements = [[123,"my name","description thing",1.78]];
-    const items = [];
-    for(const [id, name, desc, price] of elements){
-      items.push(<tr>
-                  <td>{id}</td>
-                  <td>{name}</td>
-                  <td>{desc}</td>
-                  <td>{price}</td>
-                </tr>);
-    }
+    const products = getProducts();
     return (
       <div>
     <h1>These are the products</h1>
@@ -24,7 +46,7 @@ class Products extends Component {
         <th>Description</th>
         <th>Price</th>
       </tr>
-      {items}
+      {products}
       </table>
       </div>
     );
