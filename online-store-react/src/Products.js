ff896b5432ac78bc78a78b711f4db91a7aaa05ff
josh <josh>
2020-05-18 19:31:57 -0600

    products listing stub working

--- online-store-react/src/Products.js
+++ online-store-react/src/Products.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 
 
 class Products extends Component {
+
   render() {
+    const elements = [[123,"my name","description thing",1.78]];
+    const items = [];
+    for(const [id, name, desc, price] of elements){
+      items.push(<tr>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{desc}</td>
+                  <td>{price}</td>
+                </tr>);
+    }
     return (
-      <h1>These are the products</h1>
+      <div>
+    <h1>These are the products</h1>
+      <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+      </tr>
+      {items}
+      </table>
+      </div>
     );
   }
 }
