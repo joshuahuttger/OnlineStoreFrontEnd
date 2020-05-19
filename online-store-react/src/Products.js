b400e55ae952706fceb5df27d6d1064a8dc34b6f
josh <josh>
2020-05-18 19:05:48 -0600

    Product list hello world

---
+++ online-store-react/src/Products.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Products extends Component {
+  render() {
+    return (
+      <h1>These are the products</h1>
+    );
+  }
+}
+
+export default Products;
