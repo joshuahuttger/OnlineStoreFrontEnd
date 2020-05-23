bb393d01ee0189862ac9ecc13dd975350bff2d02
josh <josh>
2020-05-22 20:06:08 -0600

    just saving

--- online-store-react/src/Products.js
+++ online-store-react/src/Products.js
@@ -1,56 +1,88 @@
 import React, { Component } from 'react';
 
-function getProductsFromAPI(){
-  const request = new XMLHttpRequest();
-  request.open('GET','http://localhost:8080/products',true);
-  request.onload = function(){
-    console.log("something here");
-    const data = JSON.parse(this.response);
-    if(request.status >= 200 && request.status < 400){
-      data.forEach(thing =>{
-        console.log(thing);
-      })
-    }else{
-      console.log("error");
-    }
-  }
-  console.log("chicken tenders");
-  return [];
-}
-
-function getProducts(){
-  const productsFromAPI = getProductsFromAPI();
-  const products = [];
-  for(const [id, name, desc, price] of productsFromAPI){
-    products.push(<tr>
-                <td>{id}</td>
-                <td>{name}</td>
-                <td>{desc}</td>
-                <td>{price}</td>
-              </tr>);
-  }
-  return products;
-}
 
 class Products extends Component {
 
-  render() {
-    const products = getProducts();
-    return (
-      <div>
-    <h1>These are the products</h1>
-      <table>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Price</th>
-      </tr>
-      {products}
-      </table>
-      </div>
-    );
-  }
+      constructor(props) {
+        super(props);
+
+        this.state = {
+          products : null
+        };
+      }
+
+      componentDidMount() {
+          this.renderMyData();
+      }
+
+      renderMyData(){
+          fetch('http://localhost:8080/products',{headers:{"Access-Control-Allow-Origin": "https://localhost"}})
+              .then((response) => response.json())
+              .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({ products : responseJson })
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+      }
+
+      render() {
+        return (
+          <div>
+        <h1>These are the products</h1>
+          <table>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+          {this.state.products}
+          </table>
+          </div>
+        );
+      }
 }
 
 export default Products;
+/*
+
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class App extends React.Component  {
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        data : null
+      };
+    }
+
+    componentWillMount() {
+        this.renderMyData();
+    }
+
+    renderMyData(){
+        fetch('https://your url')
+            .then((response) => response.json())
+            .then((responseJson) => {
+              this.setState({ data : responseJson })
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+    }
+
+    render(){
+        return(
+            <View>
+                {this.state.data ? <MyComponent data={this.state.data} /> : <MyLoadingComponnents /> }
+            </View>
+        );
+    }
+}
+*/
