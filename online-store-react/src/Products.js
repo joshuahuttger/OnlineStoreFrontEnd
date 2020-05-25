9879613e2285fbecc86c98f677727d0ecd83fa24
josh <josh>
2020-05-24 20:50:19 -0600

    rest request sort of working

--- online-store-react/src/Products.js
+++ online-store-react/src/Products.js
@@ -3,86 +3,49 @@ import React, { Component } from 'react';
 
 class Products extends Component {
 
-      constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-          products : null
-        };
-      }
+    this.state = {
+      products: null
+    };
+  }
 
-      componentDidMount() {
-          this.renderMyData();
-      }
+  componentDidMount() {
+    this.renderMyData();
+  }
 
-      renderMyData(){
-          fetch('http://localhost:8080/products',{headers:{"Access-Control-Allow-Origin": "https://localhost"}})
-              .then((response) => response.json())
-              .then((responseJson) => {
-                console.log(responseJson);
-                this.setState({ products : responseJson })
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-      }
+  renderMyData() {
+    fetch('http://localhost:8080/products')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({ products: responseJson })
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log("exiting renderMyData()");
+  }
 
-      render() {
-        return (
-          <div>
+  render() {
+    return (
+      <div>
         <h1>These are the products</h1>
-          <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-          </tr>
-          {this.state.products}
-          </table>
-          </div>
-        );
-      }
+        <table>
+          <tbody>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+            </tr>
+            {this.state.products}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Products;
-/*
-
-
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
-export default class App extends React.Component  {
-
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        data : null
-      };
-    }
-
-    componentWillMount() {
-        this.renderMyData();
-    }
-
-    renderMyData(){
-        fetch('https://your url')
-            .then((response) => response.json())
-            .then((responseJson) => {
-              this.setState({ data : responseJson })
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-    }
-
-    render(){
-        return(
-            <View>
-                {this.state.data ? <MyComponent data={this.state.data} /> : <MyLoadingComponnents /> }
-            </View>
-        );
-    }
-}
-*/
